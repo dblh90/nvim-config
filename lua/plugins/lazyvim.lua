22bb83f179715ac329580d2da72deff3f91b30a4
Hamza <hamza.hasan@emma-sleep.com>
2026-01-28 10:07:17 +0100

    add themes

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,14 +1,30 @@
 return {
     { "ellisonleao/gruvbox.nvim" },
-
+    { 'projekt0n/github-nvim-theme', name = 'github-theme' },
     {
         "embark-theme/vim",
         lazy = false,
-        priority = 1000,
         name = 'embark'
     },
-
-    -- Configure LazyVim to load gruvbox
+    {
+        "0xstepit/flow.nvim",
+        lazy = false,
+        opts = {
+                -- Your configuration options here.
+            },
+    },
+    {
+        "dgox16/oldworld.nvim",
+    },
+    {
+        "thesimonho/kanagawa-paper.nvim",
+        lazy = false,
+        opts = {},
+    },
+    {
+        "navarasu/onedark.nvim"
+    },
+    { "rose-pine/neovim", name = "rose-pine" },
     {
         "LazyVim/LazyVim",
         opts = {
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+    {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    enabled = true,
+    opts = { use_diagnostic_signs = true },
+    }
+}
